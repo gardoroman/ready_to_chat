0d9b1d0f8bcf461eb7822968f8c457a79edb98c6
gardoroman <gardoroman@gmail.com>
2021-05-03 14:31:23 -0500

    Add stun server to mix file.

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule ReadyToChat.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:uuid, "~> 1.1.7"}
+      {:uuid, "~> 1.1.7"},
+      {:stun, "1.0.43"}
     ]
   end
 
